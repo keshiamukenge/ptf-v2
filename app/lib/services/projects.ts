9b1a32c80252bb9065cc22e7dc9fc5e3d6bbc4b4
Keshia Mukenge <keshia@MBP-de-Keshia.lan>
2023-08-15 18:22:49 +0200

    fix: add dynamic api base url

--- app/lib/services/projects.ts
+++ app/lib/services/projects.ts
@@ -4,14 +4,14 @@ import { Project } from '@/app/lib/types/projects'
 import { Archive } from '@/app/lib/types/archive'
 
 export async function getSelectedWorksServices(): Promise<Project[]> {
-	const response = await fetch('http://localhost:3000/data/selectedWorks.json')
+	const response = await fetch(`${process.env.API_BASE_URL}/data/selectedWorks.json`)
 	const selectedWorksData = await response.json()
 
 	return selectedWorksData.projects
 }
 
 export async function getArchivesServices(): Promise<Archive[]> {
-	const response = await fetch('http://localhost:3000/data/archives.json')
+	const response = await fetch(`${process.env.API_BASE_URL}/data/archives.json`)
 	const archivesData = await response.json()
 
 	return archivesData.projects
