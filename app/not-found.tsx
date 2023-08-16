d9597f7ff133975fae736c0d90fb3af7a92fc089
Keshia Mukenge <keshia@MacBook-Pro-de-Keshia.local>
2023-08-16 12:59:23 +0200

    feat: add more infos on not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,6 +5,7 @@ import gsap from 'gsap'
 
 import './style.scss'
 import InternalLink from '@/app/lib/components/Links/InternalLink'
+import Header from '@/app/lib/components/Header/Header'
 import Footer from '@/app/lib/components/Footer/Footer'
 import { usePageTransitions } from '@/app/lib/providers/PageTransitionsContext'
 
@@ -22,12 +23,18 @@ export default function NotFound() {
 	}, [transitionState])
 
 	return(
-		<main ref={notFoundPageRef} className="not-found-page">
-			<h1>404</h1>
-			<div className="container-internal-link">
-				<InternalLink label="Go back to home" href="/" />
-			</div>
-			<Footer fixedPosition />
-		</main>
+		<>
+			<Header />
+			<main ref={notFoundPageRef} className="not-found-page">
+				<div className="container-error-msg">
+					<h2>Error 404</h2>
+					<h1>Page not found </h1>
+				</div>
+				<div className="container-internal-link">
+					<InternalLink label="Go back to home" href="/" />
+				</div>
+				<Footer fixedPosition />
+			</main>
+		</>
 	)
 }
